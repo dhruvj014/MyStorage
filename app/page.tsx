be65e1f2c56b19980fc882dc75ead269cc5a60f0
Dhruv Jain <dhruv.j314@gmail.com>
2025-09-17 18:14:06 -0700

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex-center h-screen">
+      <h1 className="h1">
+        {" "}
+        MyStorage - a feature-rich self-hosted storage solution.{" "}
+      </h1>
+    </div>
+  );
+}
